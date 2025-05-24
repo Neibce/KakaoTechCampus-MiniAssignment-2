572714628f765f43e2d79042de7fda46196821dc
Jun-Young Yang <y@jun0.dev>
2025-05-25 03:24:28 +0900

    Initial commit

---
+++ src/test/java/dev/jun0/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jun0.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
