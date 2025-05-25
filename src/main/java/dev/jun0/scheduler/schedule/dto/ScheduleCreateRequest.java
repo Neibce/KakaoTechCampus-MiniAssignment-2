464d53f79a089030d4cf2503aba945df2f867cd9
Jun-Young Yang <y@jun0.dev>
2025-05-25 22:56:16 +0900

    lv6 구현 완료

--- src/main/java/dev/jun0/scheduler/schedule/dto/ScheduleCreateRequest.java
+++ src/main/java/dev/jun0/scheduler/schedule/dto/ScheduleCreateRequest.java
@@ -1,12 +1,20 @@
 package dev.jun0.scheduler.schedule.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class ScheduleCreateRequest {
+    @NotBlank
+    @Size(max = 200)
     private String task;
+
+    @NotBlank
     private String authorUuid;
+
+    @NotBlank
     private String password;
 }
